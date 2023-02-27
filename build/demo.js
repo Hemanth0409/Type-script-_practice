feeea1be0711010c0b4b4b4e506ff7cd9ee85c4c
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-02-27 15:20:07 +0530

    1st commit

--- build/demo.js
+++ build/demo.js
@@ -1,1 +1,27 @@
 "use strict";
+class Emp_details {
+    constructor(emp_id, emp_name, emp_designation, emp_salary, emp_pro) {
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_designation = emp_designation;
+        this.emp_salary = emp_salary;
+        this.emp_pro = emp_pro;
+    }
+    getEmpDetails() {
+        console.log(`Emp_id:${this.emp_id}`);
+        console.log(`Emp_name:${this.emp_name}`);
+        console.log(`Emp_designation:${this.emp_designation}`);
+        console.log(`Salary:${this.emp_salary}`);
+        console.log(`Project:${this.emp_pro}`);
+    }
+    displayEmpDetails() {
+        console.log(`Emp_id:${this.emp_id}`);
+        console.log(`Emp_name:${this.emp_name}`);
+        console.log(`Emp_designation:${this.emp_designation}`);
+        console.log(`Salary:${this.emp_salary}`);
+        console.log(`Project:${this.emp_pro}`);
+    }
+}
+let empDetails = new Emp_details(101, "Hemanth", "Software developer", 50000, "webDevelopment");
+let empDetails2 = new Emp_details(101, "Hemanth", "Software developer", 500000, "webDevelopment")
+    .displayEmpDetails();
--- src/demo.ts
+++ src/demo.ts
@@ -1,15 +1,30 @@
-class emp_details{
+class Emp_details{
+
     emp_id: number;
     emp_name: string;
     emp_designation: string;
     emp_salary: number;
     emp_pro?: string|number;
   
-    constructor(emp_id: number,emp_name: string,emp_designation: string,emp_salary: number,emp_pro: string){
+    constructor(emp_id: number,emp_name: string,emp_designation: string,emp_salary: number,emp_pro?: string){
         this.emp_id=emp_id;
         this.emp_name=emp_name;
         this.emp_designation=emp_designation;
         this.emp_salary=emp_salary;
         this.emp_pro=emp_pro;
     }
+
+public getEmpDetails() : void{
+    console.log(`Emp_id:${this.emp_id}`)
+    console.log(`Emp_name:${this.emp_name}`)
+    console.log(`Emp_designation:${this.emp_designation}`)
+    console.log(`Salary:${this.emp_salary}`)
+    console.log(`Project:${this.emp_pro}`)
 }
+}
+let displayEmpDetails = new Emp_details(101,"Hemanth","Software develope",50000,"webDevelopment")
+displayEmpDetails.getEmpDetails();
+
+
+
+
